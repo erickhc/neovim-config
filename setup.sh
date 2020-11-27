15a2aadef381918c042cf328a7a5bfb6348e0fd1
Erick Hernandez Curiel <erick@hernandezcuriel.dev>
2020-11-26 20:25:09 -0600

    Remove dependency on coc

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 git submodule update --recursive --init
-mkdir $HOME/.local/share/nvim
+mkdir -p $HOME/.local/share/nvim
 ln -s $HOME/.config/nvim/site $HOME/.local/share/nvim/site
-mkdir -p site/pack/coc/start
-cd site/pack/coc/start
-curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
